f4491851b3144cea511822402a1804299b7c26ee
Giovane Guimarães <giovane.meca@gmail.com>
2023-09-07 17:58:33 -0300

    'try' inputed at Scanner.

--- basic-java/src/AboutMe.java
+++ basic-java/src/AboutMe.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 public class AboutMe {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Tell me your name.");
+            String name = scanner.next();
 
-        System.out.println("Tell me your name.");
-        String name = scanner.next();
+            System.out.println("Tell me your last name.");
+            String lastname = scanner.next();
 
-        System.out.println("Tell me your last name.");
-        String lastname = scanner.next();
+            System.out.println("Tell me your age.");
+            int age = scanner.nextInt();
 
-        System.out.println("Tell me your age.");
-        int age = scanner.nextInt();
+            System.out.println("Tell me your height (meters).");
+            double height = scanner.nextDouble();
 
-        System.out.println("Tell me your height (meters).");
-        double height = scanner.nextDouble();
+            // String name = args[0];
+            // String lastname = args[1];
+            // int age = Integer.valueOf(args[2]);
+            // double height = Double.valueOf(args[3]);
 
-        // String name = args[0];
-        // String lastname = args[1];
-        // int age = Integer.valueOf(args[2]);
-        // double height = Double.valueOf(args[3]);
-
-        System.out.println("Hi, this is " + name + " " + lastname);
-        System.out.println("I'm " + age + " y'o.");
-        System.out.println("My height is " + height + " m.");
+            System.out.println("Hi, this is " + name + " " + lastname);
+            System.out.println("I'm " + age + " y'o.");
+            System.out.println("My height is " + height + " m.");
+        }
 
     }
 }
