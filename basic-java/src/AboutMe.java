187e888dca82840699f34edf22a6113001f0ce53
Giovane Guimarães <giovane.meca@gmail.com>
2023-09-07 19:12:41 -0300

    Applying try&catch exceptions.

--- basic-java/src/AboutMe.java
+++ basic-java/src/AboutMe.java
@@ -1,31 +1,38 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
 
-        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
-            System.out.println("Tell me your name.");
-            String name = scanner.next();
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+            {
+                System.out.println("Tell me your name.");
+                String name = scanner.next();
 
-            System.out.println("Tell me your last name.");
-            String lastname = scanner.next();
+                System.out.println("Tell me your last name.");
+                String lastname = scanner.next();
 
-            System.out.println("Tell me your age.");
-            int age = scanner.nextInt();
+                System.out.println("Tell me your age.");
+                int age = scanner.nextInt();
 
-            System.out.println("Tell me your height (meters).");
-            double height = scanner.nextDouble();
+                System.out.println("Tell me your height (meters).");
+                double height = scanner.nextDouble();
 
-            // String name = args[0];
-            // String lastname = args[1];
-            // int age = Integer.valueOf(args[2]);
-            // double height = Double.valueOf(args[3]);
+                // String name = args[0];
+                // String lastname = args[1];
+                // int age = Integer.valueOf(args[2]);
+                // double height = Double.valueOf(args[3]);
 
-            System.out.println("Hi, this is " + name + " " + lastname);
-            System.out.println("I'm " + age + " y'o.");
-            System.out.println("My height is " + height + " m.");
+                System.out.println("Hi, this is " + name + " " + lastname);
+                System.out.println("I'm " + age + " y'o.");
+                System.out.println("My height is " + height + " m.");
+                scanner.close();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("The fields age and height data must be numerical inputs on US pattern.");
         }
-
     }
+
 }
